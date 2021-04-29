76da9f3adee3f599da6fc8721ee69ae426a3966c
InhuJo <inhudev@gmail.com>
2021-04-30 02:05:55 +0900

    complete page

---
+++ views/script.js
@@ -0,0 +1,98 @@
+var movies = [
+    {
+        title: "KILL BILL: <br>Volume 1",
+        director: "Quentin Tarantino",
+        starring: [
+            "Uma Thurman",
+            "Lucy Liu",
+            "Vivica A. Fox"
+        ],
+        runningTime: 111,
+        release: "October 10, 2003",
+        src: "movie_1.png"
+    },
+    {
+        title: "KILL BILL: <br>Volume 2",
+        director: "Quentin Tarantino",
+        starring: [
+            "Uma Thurman",
+            "David Carradine",
+            "Michael Madsen"
+        ],
+        runningTime: 136,
+        release: "April 8, 2004",
+        src: "movie_2.png"
+    },
+    {
+        title: "Reservoir Dogs",
+        director: "Quentin Tarantino",
+        starring: [
+           "Harvey Keitel",
+           "Tim Roth",
+           "Chris Penn"
+        ],
+        runningTime: 99,
+        release: "January 9, 1992",
+        src: "movie_3.png"
+    },
+    {
+        title: "Pulp Fiction",
+        director: "Quentin Tarantino",
+        starring: [
+           "John Travolta",
+           "Samuel L. Jackson",
+           "Uma Thurman"
+        ],
+        runningTime: 154,
+        release: "May 21, 1994",
+        src: "movie_4.jpg"
+    },
+    {
+        title: "Inglourious <br>Basterds",
+        director: "Quentin Tarantino",
+        starring: [
+           "Brad Pitt",
+           "Chrisoph Waltz",
+           "Michael Fassbender"
+        ],
+        runningTime: 153,
+        release: "May 20, 2009",
+        src: "movie_5.jpg"
+    },
+    {
+        title: "Django <br>Unchained",
+        director: "Quentin Tarantino",
+        starring: [
+           " Jamie FoxxLeonardo",
+           "Christoph Waltz",
+           "DiCaprio"
+        ],
+        runningTime: 165,
+        release: " December 11, 2012",
+        src: "movie_6.jpg"
+    },
+    {
+        title: "Inception",
+        director: "Christopher Nolan",
+        starring: [
+           "Leonardo DiCaprio",
+           "Ken Watanabe",
+           "<br>Joseph Gordon-Levitt"
+        ],
+        runningTime: 148,
+        release: "July 8, 2010",
+        src: "movie_7.jpg"
+    },
+    {
+        title: "The Dark Knight",
+        director: "Christopher Nolan",
+        starring: [
+           "Christian Bale",
+           "Michael Caine",
+           "Heath Ledger"
+        ],
+        runningTime: 152,
+        release: "July 14, 2008",
+        src: "movie_8.jpg"
+    }
+];
